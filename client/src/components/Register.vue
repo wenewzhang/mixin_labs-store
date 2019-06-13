717b92b02a6e86bceb3f8d2cc6e94ec84f3f8140
wenewzhang <wenewboy@gmail.com>
2019-06-14 07:47:35 +0800

    first commit

---
+++ client/src/components/Register.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <h1>Register</h1>
+  <input v-model="email" type="email" name="email" placeholder="email"/><br>
+  <input v-model="password" type="password" name="password" placeholder="password"/><br>
+  <button @click="register">Register</button>
+</div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log('reg:', this.email, this.password)
+      console.log(response.data)
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.email = 'hello world'
+    }, 2000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+app.post('/register', (req, res) => {
+  res.send({
+    message: `hello,${req.body.email}!!`
+  })
+})
+
+app.listen(process.env.PORT || 8081)
