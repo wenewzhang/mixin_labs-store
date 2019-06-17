e86f8b82f947d1efd3f577e6f3ed83bb9bac8b60
wenewzhang <wenewboy@gmail.com>
2019-06-17 14:32:56 +0800

    vuetify input and button

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,10 +1,34 @@
 <template>
-<div>
-  <h1>{{ email }}</h1>
-  <input v-model="email" type="email" name="email" placeholder="email"/><br>
-  <input v-model="password" type="password" name="password" placeholder="password"/><br>
-  <button @click="register">Register</button>
-</div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <panel title="Register">
+        <form
+          name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <br>
+        <div class="danger-alert" v-html="error" />
+        <br>
+        <v-btn
+          dark
+          class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </panel>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from '@/components/Register'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Register',
+      path: '/register',
+      name: 'register',
       component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
