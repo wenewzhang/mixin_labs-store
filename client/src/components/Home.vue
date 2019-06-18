166263e2c008bab4c42d32da21b2170c5c29016b
wenewzhang <wenewboy@gmail.com>
2019-06-18 14:43:24 +0800

    css

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -26,3 +26,20 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.hooper-slide {
+ background-color:#62caaa;
+ padding:20px;
+ display:flex;
+ justify-content:center;
+ align-items:center;
+ color:#fff;
+ border:2px solid #fff;
+ font-size:30px;
+ border-radius:10px;
+}
+.is-active {
+ background-color:#47da7f;
+}
+</style>
