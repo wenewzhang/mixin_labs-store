74f93c45846c1b629a6b4e546532aa146ed8558d
wenewzhang <wenewboy@gmail.com>
2019-06-22 16:25:57 +0800

    add fontawesome icon & material icon

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <font-awesome-icon icon="user-secret" />
+    <font-awesome-icon icon="spinner" />
+    <font-awesome-icon icon="align-left" />
     <page-header />
     <router-view/>
   </div>
--- client/src/main.js
+++ client/src/main.js
@@ -5,10 +5,24 @@ import router from './router'
 import store from '@/store/store'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSpinner, faAlignLeft } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSpinner, faAlignLeft)
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
-Vue.use(Vuetify)
+// Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  iconfont: 'md'
+})
 sync(store, router)
 
 new Vue({
