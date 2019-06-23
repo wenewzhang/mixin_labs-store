cd70609060fb6fa0723c6ca09ef7310b078e520d
wenewzhang <wenewboy@gmail.com>
2019-06-23 08:44:58 +0800

    v-select

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <font-awesome-icon icon="user-secret" />
     <font-awesome-icon icon="spinner" />
     <font-awesome-icon icon="align-left" />
--- client/src/components/Payment.vue
+++ client/src/components/Payment.vue
@@ -1,24 +1,31 @@
 <template>
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
-      <div class="danger-alert" v-html="error" />
-      <br>
       <v-flex xs9>
         <v-card white color="primary">
-          <select v-model="selected">
-            <option disabled value="">Please select one</option>
-            <option>mixin</option>
-            <option>deposit</option>
-          </select>
-          <span>Selected: {{ selected }}</span>
-        </v-card>
-      </v-flex>
-        <v-flex xs9>
-        <v-card white color="primary">
-          <v-btn color="#ffee00" @click="pay">Pay</v-btn>
+          <div class="danger-alert" v-html="error" />
+          <p>{{encodedStr}}</p>
           <p>{{decodedStr}}</p>
         </v-card>
       </v-flex>
+      <v-flex xs9>
+        <v-card white color="primary">
+        <v-select v-model="paymentMethod"
+          :items="items"
+          item-text="source"
+          box
+          persistent-hint
+          return-object
+          single-line
+          label="Please select one payment method:"
+        ></v-select>
+        </v-card>
+      </v-flex>
+      <v-flex xs9>
+      <v-card white color="primary">
+        <v-btn color="#ffee00" @click="pay">Pay</v-btn>
+      </v-card>
+    </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -29,8 +36,12 @@ import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
     return {
-      selected: 'mixin',
-      decodedStr: '',
+      paymentMethod: null,
+      decodedStr: null,
+      encodedStr: null,
+      items: [ {value: 'mixin', source: 'mixin'},
+               {value: 'deposit', source: 'deposit'},
+             ],
       error: null
     }
   },
@@ -38,12 +49,13 @@ export default {
   methods: {
     async pay () {
       try {
+        // console.log(this.paymentMethod.source)
         const response = await AuthenticationService.pay({
-          source: this.selected
+          source: this.paymentMethod.source
         })
         if (response.status === 200) {
           if (response.data.source === 'deposit') {
-            // console.log(response.data)
+            this.encodedStr = response.data.pay
             this.decodedStr = atob(response.data.pay)
           } else {
             var decodedStr = atob(response.data.pay)
