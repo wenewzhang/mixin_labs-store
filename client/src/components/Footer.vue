e0a0403eb85ab7c4fc887f30eb08d6d0137131de
wenewzhang <wenewboy@gmail.com>
2019-06-24 16:40:45 +0800

    footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,21 +1,48 @@
 <template>
-  <footer>
-    <div class="container">
-      <router-link class="logo-font" :to="{ name: 'home', params: {} }">
-        conduit
-      </router-link>
-      <span class="attribution">
-        An interactive learning project from
-        <a rel="noopener noreferrer" target="blank" href="https://thinkster.io"
-          >Thinkster</a
-        >. Code &amp; design licensed under MIT.
-      </span>
-    </div>
-  </footer>
+  <v-footer
+    dark
+    height="auto"
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-3"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;2018 — <strong>Vuetify</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
 </template>
 
+
 <script>
-export default {
-  name: "Footer"
-};
+  export default {
+    name: "Footer",
+    data: () => ({
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
+      ]
+    })
+  }
 </script>
